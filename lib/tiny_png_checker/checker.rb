2203243551e514fa22429e038f5a3debfb998ea6
Damián Arrillaga <damian.arrillaga@gmail.com>
2018-09-06 16:46:56 -0300

    Add file compressor, small refactor

--- lib/tiny_png_checker/checker.rb
+++ lib/tiny_png_checker/checker.rb
@@ -1,20 +1,20 @@
-require "chunky_png"
+require 'tiny_png_checker/utils/image_files_retriever'
+require 'tiny_png_checker/utils/tiny_png'
 
 module TinyPngChecker
   class Checker
 
-    def check_tiny_png_on_folders(folders)
-      png_files = folders.flat_map { |base_dir| Dir.glob("#{File.expand_path(base_dir)}/**/*.png")}
+    def check_tiny_png_on_folders(folders = [])
+      png_files = Utils::ImageFilesRetriever.get_png_files_from_folders(folders)
 
-      not_processed_files = png_files.select do |file|
-        image = ChunkyPNG::Image.from_file(file)
-        image.metadata['TinyPNGProcessedFlag'].nil?
-      end
+      not_processed_files = Utils::TinyPng.not_processed_files(png_files)
 
       if not_processed_files.any?
         $stderr.puts("Files not processed by TinyPNG:")
         $stderr.puts(not_processed_files)
         abort
+      else
+        $stdout.puts("No files to process")
       end
     end
 
